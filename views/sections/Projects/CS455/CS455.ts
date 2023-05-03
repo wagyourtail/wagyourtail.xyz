292618abe5bfcf21d010461b67a115e3d6823570
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-05-02 18:20:33 -0700

    M4-PR1

---
+++ views/sections/Projects/CS455/CS455.ts
@@ -0,0 +1,4 @@
+import { PageListener } from "../../../../sitepage";
+import { Express } from "express";
+
+export const page = new PageListener("CS455 - AI", "Projects from the class I took on Artificial Intelligence. (in Unity).");
